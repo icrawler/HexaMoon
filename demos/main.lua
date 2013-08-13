f258ac3016817c62aceedc8728a7902963aa4cbf
icrawler <phoenixcc.enero@yahoo.com>
2013-08-13 14:05:41 +0800

    Added a Love2D demo, fixed lots of typos, added other stuff.

---
+++ demos/main.lua
@@ -0,0 +1,31 @@
+-- HexaMoon Demo (uses Love2D)
+
+HXM = require "HexaMoon"
+
+function love.load()
+	width = 6
+	height = 6
+	hexGrid = HXM.createRectGrid(width, height, {color={255, 255, 255}})
+end
+
+function love.update()
+
+end
+
+function love.draw()
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.setLine(5)
+	for i=1, height do
+		for j=1, math.ceil(width + height/2 - 1) do
+			if hexGrid[i][j] ~= nil then
+				local vertices = HXM.getHexVertices(20, 0, 0)
+				love.graphics.polygon("fill", vertices[1].x, vertices[1].y,
+											  vertices[2].x, vertices[2].y,
+											  vertices[3].x, vertices[3].y,
+											  vertices[4].x, vertices[4].y,
+											  vertices[5].x, vertices[5].y,
+											  vertices[6].x, vertices[6].y)
+			end
+		end
+	end
+end
---
+++ demos/run.bat
@@ -0,0 +1,1 @@
+start "" "C:\Program Files (x86)\LOVE\love.exe" "%CD%" -- console
