df94e60ed8b751d57064e063fe74e6e8d714dc83
icrawler <phoenixcc.enero@yahoo.com>
2013-08-13 13:27:09 +0800

    Updated Readme, added function for creating rectangular-shaped hex grids

--- HexaMoon.lua
+++ HexaMoon.lua
@@ -1,14 +1,54 @@
 -- HexaMoon, a Hexagonal Grid library for Lua
--- License: See License File
+
+-- Hexagonal Grid Library for Lua
+-- Copyright (C) 2013  Phoenix Enero
+
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
 
 local hxm = {}
 local hxm.grid = {}
 
-function hxm.grid:new(o)
-	o = o or {}
-	setmetatable(o, self)
-	self.__index = self
-	return o
+
+-- Gets the Hexagon vertices for radius (distance from center to corner) and x/y coordinates of the center
+function hxm.getHexVertices(rad, cx, cy)
+	local vertices = {}
+	for i=1, 6 do
+		local angle = 2*math.pi * (i+0.5)
+		local x_i = cx + size * math.cos(angle)
+		local y_i = cy + size * math.sin(angle)
+		table.insert(vertices, {x=x_i, y=y_i})
+	end
+	return vertices
+end
+
+-- Creates rectangular shaped grid with origin at top left corner
+function hxm.createRectGrid(width, height, obj)
+	obj = obj or {__val=true}
+	local grid = {}
+	for i=1, height do
+		grid[i] = {}
+		for j=1, math.ceil(width + height/2 - 1) do
+			if math.ceil(j + i/2 - 1) < math.ceil(width + height/2 - 1) then
+				grid[i][j] = nil
+			else
+				grid[i][j] = obj
+			end
+		end
+	end
+end
+
+-- Creates hexagonal shaped grid with origin at center
+function hxm.createHexGrid(radius)
+
 end
 
 return hxm
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ HexaMoon
 Hexagonal Grid Library for Lua
 
 This library is written in PURE Lua, which means it can be used with any framework :D
+
+This library implements Axial Coordinate hex grids with the hexagon oriented such that it is "pointy topped".
